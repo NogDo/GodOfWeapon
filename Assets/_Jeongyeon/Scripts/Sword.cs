6073d4edd018a3800a260270d7c6a360172bfe27
이정연 <lkw930@naver.com>
2024-08-27 21:37:58 +0900

    08-27 무기 찌르기 공격 구현 완료

--- Assets/_Jeongyeon/Scripts/Sword.cs
+++ Assets/_Jeongyeon/Scripts/Sword.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Sword : MonoBehaviour
@@ -8,85 +9,121 @@ public class Sword : MonoBehaviour
     public float AttackRange;
     public LayerMask targetLayer;
     public bool isAttacking = false;
+    public int monsterIndex;
+    public float coolTime = 1.0f;
     #endregion
 
     #region private Fields
-
     private Vector3 startPosition;
     private Vector3 endRotatePosition;
+    private Quaternion startRotation;
     private Transform enemyTransform;
+    private GameObject attackParent;
+    private GameObject startParent;
     #endregion
 
-    private void Awake()
+    private void Start()
     {
-       
-    }
+        startParent = gameObject.transform.parent.gameObject;
+        attackParent = new GameObject();
+        attackParent.transform.position = gameObject.transform.parent.position;
+        attackParent.transform.rotation = gameObject.transform.parent.rotation;
+        attackParent.transform.localScale = gameObject.transform.parent.localScale;
+        
+    }   
     private void Update()
     {
         FindTarget();
     }
+    /// <summary>
+    /// 먼저 적을 찾고 공격을 준비하는 함수
+    /// </summary>
     private void FindTarget()
     {
         Collider[] target = Physics.OverlapSphere(transform.position, AttackRange, targetLayer);
-
+ 
         if (isAttacking == false && target.Length != 0)
         {
-            enemyTransform = target[0].transform;
-            transform.LookAt(enemyTransform.position); 
-            Debug.Log(transform.localRotation);
-            StartCoroutine(PrepareAttack(enemyTransform));
+            if (target.Length == 1)
+            {
+                enemyTransform = target[0].transform;
+            }
+            else if (monsterIndex > target.Length)
+            {
+                monsterIndex = target.Length - 1;
+                enemyTransform = target[monsterIndex].transform;
+            }
+            else
+            {
+                enemyTransform = target[monsterIndex].transform;
+            }
+            Vector3 postion = enemyTransform.position - gameObject.transform.position;
+            gameObject.transform.forward = postion;
+
+           float setY = transform.localRotation.eulerAngles.y;
+            if (setY > 180)
+            {
+                setY -= 360.0f;
+                startRotation.y = setY;
+                
+            }
+            else { startRotation.y = setY; }
+            StartCoroutine(PrepareAttack(setY));
         }
         else
         {
+            
             return;
         }
     }
 
-    private IEnumerator PrepareAttack(Transform ememyTransform)
+    /// <summary>
+    /// 무기가 각도를 잡는 함수
+    /// </summary>
+    /// <param name="setY">eulerAngels을 담는 변수</param>
+    /// <returns></returns>
+    private IEnumerator PrepareAttack(float setY)
     {
+        attackParent.transform.position = startParent.transform.position;
+        attackParent.transform.rotation = startParent.transform.rotation;
+        gameObject.transform.parent = attackParent.transform;
         isAttacking = true;
-        Debug.Log(transform.localRotation);
-        Quaternion startRotation = transform.localRotation;
-        Quaternion endRotation = Quaternion.Euler(90, transform.localRotation.y, transform.localRotation.z);
-        startPosition = transform.localPosition;
-        endRotatePosition = new Vector3(transform.localPosition.x, transform.localPosition.y, transform.localPosition.z - 2);
-
         float time = 0.0f;
-        float duration = 0.3f;
+        float duration = 0.5f;
         while (time <= duration)
         {
+            transform.localRotation = Quaternion.Slerp(transform.localRotation, Quaternion.Euler(90, setY, 0), time / duration);
             transform.localPosition = Vector3.Lerp(startPosition, endRotatePosition, time / duration);
-            transform.localRotation = Quaternion.Slerp(startRotation, endRotation, time / duration);
             time += Time.deltaTime;
             yield return null;
         }
-        yield return new WaitForSeconds(0.2f);
+        transform.localRotation = Quaternion.Euler(90, setY, 0);
         transform.localPosition = endRotatePosition;
-        transform.localRotation = endRotation;
-        StartCoroutine(StartAttack(transform));
+        StartCoroutine(StartAttack());
         yield return null;
     }
 
-    private IEnumerator StartAttack(Transform startPosition)
+    private IEnumerator StartAttack()
     {
 
         float time = 0.0f;
         float duration = 0.3f;
-        transform.localRotation = startPosition.localRotation;
+        enemyTransform.position = new Vector3(enemyTransform.position.x, 0, enemyTransform.position.z);
+        // Debug.Log(rotation.y);
         while (time <= duration)
         {
-            transform.localPosition = Vector3.Lerp(startPosition.localPosition, enemyTransform.localPosition, time / duration);
+            transform.position = Vector3.Lerp(transform.position, enemyTransform.position, time / duration);
             time += Time.deltaTime;
             yield return null;
         }
-        transform.localPosition = enemyTransform.localPosition;
-        yield return new WaitForSeconds(0.5f);
+        transform.position = enemyTransform.localPosition;
         StartCoroutine(EndAttack(transform));
         yield return null;
     }
 
     private IEnumerator EndAttack(Transform startPostion)
     {
+        gameObject.transform.parent = startParent.transform;
         float time = 0.0f;
         float duration = 0.1f;
         while (time <= duration)
@@ -98,7 +135,7 @@ public class Sword : MonoBehaviour
         }
         transform.localPosition = new Vector3(0, 0, 0);
         transform.localRotation = Quaternion.Euler(0, 0, 0);
-        yield return new WaitForSeconds(0.3f);
+        yield return new WaitForSeconds(coolTime);
         isAttacking = false;
         yield return null;
     }
