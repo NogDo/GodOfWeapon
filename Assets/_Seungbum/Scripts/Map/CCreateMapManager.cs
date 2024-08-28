e8617242d4bac2f1b098b94e13023eeef212d13f
NogDo <ssb9292@naver.com>
2024-08-28 19:08:18 +0900

    08.28_학원 맵 끝에 울타리 동적 생성 완료

--- Assets/_Seungbum/Scripts/Map/CCreateMapManager.cs
+++ Assets/_Seungbum/Scripts/Map/CCreateMapManager.cs
@@ -13,6 +13,7 @@ public class CCreateMapManager : MonoBehaviour
     #endregion
 
     #region private 변수
+    [Header("바닥 관련")]
     [SerializeField]
     GameObject oStartFloor;
     [SerializeField]
@@ -22,14 +23,20 @@ public class CCreateMapManager : MonoBehaviour
     [SerializeField]
     GameObject[] oTraps;
 
+    [Header("울타리 관련")]
+    [SerializeField]
+    GameObject[] oFence;
+    [SerializeField]
+    GameObject oFenceEdge;
+
     GameObject tfMapParent;
 
     float fFloorWidth = 4.0f;
     float fFloorHeight = 4.0f;
 
-    int nBasicFloorPercent = 70;
+    int nBasicFloorPercent = 75;
     int nSpecFloorPercent = 20;
-    int nTrapPercent = 10;
+    int nTrapPercent = 5;
     #endregion
 
     private void Awake()
@@ -41,17 +48,17 @@ public class CCreateMapManager : MonoBehaviour
 
     private void Start()
     {
-        CreateFloor(-1, 3, -1, 3);
+        CreateMap(-1, 5, -1, 10);
     }
 
     /// <summary>
-    /// 바닥을 생성한다.
+    /// 맵을 생성한다.
     /// </summary>
     /// <param name="minX">가로 최소값</param>
     /// <param name="maxX">가로 최대값</param>
     /// <param name="minZ">세로 최소값</param>
     /// <param name="maxZ">세로 최대값</param>
-    public void CreateFloor(int minX, int maxX, int minZ, int maxZ)
+    public void CreateMap(int minX, int maxX, int minZ, int maxZ)
     {
         tfMapParent = GameObject.Find("Map");
 
@@ -59,59 +66,153 @@ public class CCreateMapManager : MonoBehaviour
         {
             for (int j = minZ; j < maxZ; j++)
             {
-                if (i == 0 && j == 0)
-                {
-                    Instantiate(oStartFloor, Vector3.zero, Quaternion.identity, tfMapParent.transform);
-
-                    continue;
-                }
-
-                int randFloorType = Random.Range(0, 100);
-                int randFloor = 0;
-
-                if (randFloorType < nBasicFloorPercent)
-                {
-                    randFloor = Random.Range(0, oBasicFloors.Length);
-
-                    Instantiate
-                        (
-                            oBasicFloors[randFloor], 
-                            new Vector3(i * fFloorWidth, 0.0f, j * fFloorHeight), 
-                            Quaternion.identity, 
-                            tfMapParent.transform
-                        );
-                }
-
-                else if (randFloorType < nBasicFloorPercent + nSpecFloorPercent)
-                {
-                    randFloor = Random.Range(0, oSpecFloors.Length);
-
-                    Instantiate
-                        (
-                            oSpecFloors[randFloor],
-                            new Vector3(i * fFloorWidth, 0.0f, j * fFloorHeight),
-                            Quaternion.identity,
-                            tfMapParent.transform
-                        );
-                }
-
-                else
-                {
-                    randFloor = Random.Range(0, oTraps.Length);
-
-                    Instantiate
-                        (
-                            oTraps[randFloor],
-                            new Vector3(i * fFloorWidth, 0.0f, j * fFloorHeight),
-                            Quaternion.identity,
-                            tfMapParent.transform
-                        );
-                }
-
-                
+                CreateFloor(i, j);
             }
         }
 
+        CreateFence(minX, maxX, minZ, maxZ);
+
         tfMapParent.transform.Rotate(new Vector3(0.0f, 45.0f, 0.0f));
+    }
+
+    /// <summary>
+    /// 바닥을 생성한다.
+    /// </summary>
+    /// <param name="posX">X 위치</param>
+    /// <param name="posZ">Z 위치</param>
+    void CreateFloor(int posX, int posZ)
+    {
+        if (posX == 0 && posZ == 0)
+        {
+            Instantiate(oStartFloor, Vector3.zero, Quaternion.identity, tfMapParent.transform);
+
+            return;
+        }
+
+        int randFloorType = Random.Range(0, 100);
+        int randFloor = 0;
+
+        if (randFloorType < nBasicFloorPercent)
+        {
+            randFloor = Random.Range(0, oBasicFloors.Length);
+
+            Instantiate
+                (
+                    oBasicFloors[randFloor],
+                    new Vector3(posX * fFloorWidth, 0.0f, posZ * fFloorHeight),
+                    Quaternion.identity,
+                    tfMapParent.transform
+                );
+        }
+
+        else if (randFloorType < nBasicFloorPercent + nSpecFloorPercent)
+        {
+            randFloor = Random.Range(0, oSpecFloors.Length);
+
+            Instantiate
+                (
+                    oSpecFloors[randFloor],
+                    new Vector3(posX * fFloorWidth, 0.0f, posZ * fFloorHeight),
+                    Quaternion.identity,
+                    tfMapParent.transform
+                );
+        }
+
+        else
+        {
+            randFloor = Random.Range(0, oTraps.Length);
+
+            Instantiate
+                (
+                    oTraps[randFloor],
+                    new Vector3(posX * fFloorWidth, 0.0f, posZ * fFloorHeight),
+                    Quaternion.identity,
+                    tfMapParent.transform
+                );
+        }
+    }
+
+    /// <summary>
+    /// 울타리를 생성한다.
+    /// </summary>
+    /// <param name="minX">가로 최소값</param>
+    /// <param name="maxX">가로 최대값</param>
+    /// <param name="minZ">세로 최소값</param>
+    /// <param name="maxZ">세로 최대값</param>
+    void CreateFence(int minX, int maxX, int minZ, int maxZ)
+    {
+        for (int i = minX; i < maxX; i++)
+        {
+            int randFence = Random.Range(0, oFence.Length);
+
+            Instantiate
+                (
+                    oFence[randFence],
+                    new Vector3(i * fFloorWidth, 0.0f, minZ * fFloorHeight),
+                    Quaternion.identity,
+                    tfMapParent.transform
+                );
+
+            Instantiate
+                (
+                    oFence[randFence],
+                    new Vector3(i * fFloorWidth, 0.0f, maxZ * fFloorHeight),
+                    Quaternion.identity,
+                    tfMapParent.transform
+                );
+        }
+
+        for (int i = minZ; i < maxZ; i++)
+        {
+            int randFence = Random.Range(0, oFence.Length);
+
+            Instantiate
+                (
+                    oFence[randFence],
+                    new Vector3(minX * fFloorWidth, 0.0f, i * fFloorHeight),
+                    Quaternion.Euler(0.0f, -90.0f, 0.0f),
+                    tfMapParent.transform
+                );
+
+            Instantiate
+                (
+                    oFence[randFence],
+                    new Vector3(maxX * fFloorWidth, 0.0f, i * fFloorHeight),
+                    Quaternion.Euler(0.0f, -90.0f, 0.0f),
+                    tfMapParent.transform
+                );
+        }
+
+        Instantiate
+               (
+                   oFenceEdge,
+                   new Vector3(minX * fFloorWidth, 0.0f, minZ * fFloorHeight),
+                   Quaternion.identity,
+                   tfMapParent.transform
+               );
+
+        Instantiate
+               (
+                   oFenceEdge,
+                   new Vector3(maxX * fFloorWidth, 0.0f, minZ * fFloorHeight),
+                   Quaternion.identity,
+                   tfMapParent.transform
+               );
+
+        Instantiate
+               (
+                   oFenceEdge,
+                   new Vector3(minX * fFloorWidth, 0.0f, maxZ * fFloorHeight),
+                   Quaternion.identity,
+                   tfMapParent.transform
+               );
+
+        Instantiate
+               (
+                   oFenceEdge,
+                   new Vector3(maxX * fFloorWidth, 0.0f, maxZ * fFloorHeight),
+                   Quaternion.identity,
+                   tfMapParent.transform
+               );
     }
 }
