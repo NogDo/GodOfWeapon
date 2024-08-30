53c71f7e962f4fec4533c8b3b16d968441554762
Paul9707 <157686234+Paul9707@users.noreply.github.com>
2024-08-30 13:17:37 +0900

    단검 애니메이션 수정 및 공격 모션 수정 완료

--- Assets/_Jeongyeon/Scripts/Controller/Sword/ShortSwordController.cs
+++ Assets/_Jeongyeon/Scripts/Controller/Sword/ShortSwordController.cs
@@ -20,8 +20,32 @@ public class ShortSwordController : SwordController
     }
     public override void Update()
     {
-        base.Update();
-        
+       base.Update();
+    }
+
+    public override IEnumerator PreParePierce(float setY)
+    {
+        attackParent.transform.position = startParent.transform.position;
+        attackParent.transform.rotation = startParent.transform.rotation;
+        gameObject.transform.parent = attackParent.transform;
+
+        endRotatePosition = transform.localRotation * (Vector3.forward) * -1.0f;
+        isAttacking = true;
+        float time = 0.0f;
+        float duration = 0.4f;
+
+        while (time <= duration)
+        {
+            transform.localRotation = Quaternion.Slerp(transform.localRotation, Quaternion.Euler(-55, setY, 0), time / duration);
+            transform.localPosition = Vector3.Lerp(startPosition, endRotatePosition, time / duration);
+            time += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.localRotation = Quaternion.Euler(-55, setY, 0);
+        transform.localPosition = endRotatePosition;
+        StartCoroutine(Pierce());
+        yield return null;
     }
     public override IEnumerator Pierce()
     {
