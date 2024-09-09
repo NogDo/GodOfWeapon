ee019c3159281e442c3dd15fa5c159baed45a131
NogDo <ssb9292@naver.com>
2024-09-09 16:05:37 +0900

    09.09_학원 [New] 상자가 생성될 때 파티클과 애니메이션 재생

--- Assets/_Seungbum/Scripts/Enemy/CEnemyChestController.cs
+++ Assets/_Seungbum/Scripts/Enemy/CEnemyChestController.cs
@@ -9,23 +9,41 @@ using SystemRandom = System.Random;
 public class CEnemyChestController : MonoBehaviour, IHittable
 {
     #region private 변수
+    [SerializeField]
+    ParticleSystem particleSpawnPrefab;
 
+    ParticleSystem particleSpawn;
+
+    Collider col;
+    Animator animator;
     #endregion
 
     void OnEnable()
     {
+        col = GetComponent<Collider>();
+        animator = GetComponent<Animator>();
+
+        col.enabled = false;
+
         float randX = Random.Range((CCreateMapManager.Instance.MapSize.minX + 1) * 4.0f, CCreateMapManager.Instance.MapSize.maxX * 4.0f);
         float randZ = Random.Range((CCreateMapManager.Instance.MapSize.minZ + 1) * 4.0f, CCreateMapManager.Instance.MapSize.maxZ * 4.0f);
 
         Vector3 spawnPoint = new Vector3(randX, 7.65f, randZ);
+        Vector3 particlePoint = new Vector3(randX, 0.2f, randZ);
 
-        transform.localPosition = spawnPoint;
+        particleSpawn = Instantiate(particleSpawnPrefab, particlePoint, Quaternion.identity);
+        particleSpawn.Play();
+
+        transform.position = spawnPoint;
+
+        Invoke("AnimationStart", 1.0f);
     }
 
     public void Die()
     {
         // TODO : 여기에 상자 부셔져서 파편 날리는 기능 구현하면 됨.
 
+        Destroy(particleSpawn.gameObject);
         Destroy(gameObject);
     }
 
@@ -37,5 +55,14 @@ public class CEnemyChestController : MonoBehaviour, IHittable
     [Obsolete("mass값을 파라미터로 받는 Hit(float damage, float mass)를 사용하세요.")]
     public void Hit(float damage)
     {
+    }
+
+    /// <summary>
+    /// 애니메이션을 재생하고 콜라이더를 활성화 시킨다.
+    /// </summary>
+    void AnimationStart()
+    {
+        animator.SetTrigger("Down");
+        col.enabled = true;
     }
 }
