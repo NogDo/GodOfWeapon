ea9fa7d18dcef7e91f522020c240a1d54d3d9128
이정연 <lkw930@naver.com>
2024-09-19 18:42:19 +0900

    09-19 학원 중간 정산 인벤토리 코드 작성완료(아이템이 들어오면 생성후 데이터 입력)

--- Assets/_Jeongyeon/Scripts/Weapon/Info/WeaponStatInfo.cs
+++ Assets/_Jeongyeon/Scripts/Weapon/Info/WeaponStatInfo.cs
@@ -5,19 +5,47 @@ using UnityEngine;
 
 public class WeaponStatInfo : MonoBehaviour
 {
-
+    #region Public Fields
     public WeaponData data;
     public int index;
+    #endregion
+    #region Private Fields
+    #endregion
+
 
     public void Init(WeaponData data, int index)
     {
         this.data = data;
         this.index = index;
-
     }
-    protected void LWeaponSetValue(int level)
+    public void LWeaponSetValue(int level)
     {
         data.damage += data.damage * 0.25f * (level - 1);
+        data.attackSpeed -= data.attackSpeed * 0.07f * (level - 1);
+    }
+    public void SWeaponSetValue(int level)
+    {
+        data.damage += data.damage * 0.25f * (level - 1);
+        data.attackSpeed -= 0.1f * (level - 1);
+    }
+
+    public void CrossbowSetValue(int level)
+    {
+        switch (level - 1)
+        {
+            case 0:
+                break;
+            case 1:
+                data.damage += data.damage + 3;
+                break;
+            case 2:
+                data.damage += data.damage + 5;
+                break;
+            case 3:
+                data.damage += data.damage + 12;
+                break;
+        }
+        data.attackSpeed -= data.attackSpeed * 0.06f * (level - 1);
     }
 }
 
