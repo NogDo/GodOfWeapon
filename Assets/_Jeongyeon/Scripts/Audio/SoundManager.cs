a4253db9aa37c22d95d03494fdc298aa7f79cac5
이정연 <lkw930@naver.com>
2024-10-08 15:25:58 +0900

    10-08 학원 - (추가)사운드 작업 기초 완료

--- Assets/_Jeongyeon/Scripts/Audio/SoundManager.cs
+++ Assets/_Jeongyeon/Scripts/Audio/SoundManager.cs
@@ -6,12 +6,15 @@ public class SoundManager : MonoBehaviour
 {
     public static SoundManager Instance { get; private set; }
 
+    [Header("사운드 클립을 담아두는 배열")]
     public AudioClip[] backgrounAudioClip;
+    public AudioClip[] effectAudioClip;
+    public AudioClip[] characterAudioClip;
 
-
+    [Header("사운드별 오디오 소스")]
     public AudioSource backgroundAudioSource;
     public AudioSource effectAudioSource;
-    public AudioSource playerAudioSource;
+    public AudioSource characterAudioSource;
     private void Awake()
     {
         if (Instance == null)
@@ -22,18 +25,65 @@ public class SoundManager : MonoBehaviour
         
     }
 
-    private void Start()
+    private IEnumerator Start()
     {
         PlayBackgrounAudio(0);
+        yield return new WaitUntil(() => CStageManager.Instance != null);
+        CStageManager.Instance.OnStageEnd += StageEndSound;
     }
 
     public void PlayBackgrounAudio(int index)
     {
-        if(backgroundAudioSource != null && backgroundAudioSource.isPlaying)
+        StopBackgroundAudio();
+        backgroundAudioSource.clip = backgrounAudioClip[index];
+        backgroundAudioSource.Play();
+    }
+
+    public void PlayEffectAudio(int index)
+    {
+        StopEffectAudio();
+        effectAudioSource.clip = effectAudioClip[index];
+        effectAudioSource.Play();
+    }
+
+    public void PlayCharacterAudio(int index)
+    {
+        StopCharacterAudio();
+        characterAudioSource.clip = characterAudioClip[index];
+        characterAudioSource.Play();
+    }
+
+
+    public void StopBackgroundAudio()
+    {
+        if (backgroundAudioSource.clip != null && backgroundAudioSource.isPlaying)
         {
             backgroundAudioSource.Stop();
         }
-        backgroundAudioSource.clip = backgrounAudioClip[index];
-        backgroundAudioSource.Play();
+    }
+
+    public void StopEffectAudio()
+    {
+        if (effectAudioSource.clip != null && effectAudioSource.isPlaying)
+        {
+            effectAudioSource.Stop();
+        }
+    }
+
+    public void StopCharacterAudio()
+    {
+        if (characterAudioSource.clip != null && characterAudioSource.isPlaying)
+        {
+            characterAudioSource.Stop();
+        }
+    }
+
+    public void StageEndSound()
+    {
+        StopBackgroundAudio();
+        StopCharacterAudio();
+        StopCharacterAudio();
+        PlayBackgrounAudio(3);
+
     }
 }
--- Assets/_Jeongyeon/Scripts/Scene/ChangeScene.cs
+++ Assets/_Jeongyeon/Scripts/Scene/ChangeScene.cs
@@ -7,7 +7,6 @@ using UnityEngine.SceneManagement;
 public class ChangeScene : MonoBehaviour
 {
    
-    
     public void ChangeGameScene()
     {
         SceneManager.LoadSceneAsync("MapCreate");
@@ -16,7 +15,7 @@ public class ChangeScene : MonoBehaviour
 
     public void GameExit()
     {
-       // Application.Quit(); // 게임 종료호출 메서드 단, 에디터에서는 작동하지 않고 빌드된 상태에서만 작동
-        EditorApplication.ExitPlaymode(); // 게임 종료호출 메서드 단, 빌드된 상태에서는 작동하지 않고 에디터에서만 작동
+        // Application.Quit(); // 게임 종료호출 메서드 단, 에디터에서는 작동하지 않고 빌드된 상태에서만 작동
+         // 게임 종료호출 메서드 단, 빌드된 상태에서는 작동하지 않고 에디터에서만 작동
     }
 }
